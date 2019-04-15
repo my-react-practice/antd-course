6824b838f2c0f7fcfc7981dd712155ab9369a9d5
lizhigaomac <lizhigao@021.com>
2019-04-15 23:46:00 +0800

    init model

--- pages/puzzlecards.js
+++ pages/puzzlecards.js
@@ -1,55 +1,51 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'antd';
+import { connect } from 'dva';
 
-export default class PuzzleCardsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.counter = 100;
-    this.state = {
-      cardList: [
-        {
-          id: 1,
-          setup: 'Did you hear about the two silk worms in a race?',
-          punchline: 'It ended in a tie'
-        },
-        {
-          id: 2,
-          setup: "What happens to a frog's car when it breaks down?",
-          punchline: 'It gets toad away'
-        }
-      ]
-    };
-  }
+const namespace = 'puzzlecards';
 
-  addNewCard = () => {
-    this.setState(prevState => {
-      const prevCardList = prevState.cardList;
-      this.counter += 1;
-      const card = {
-        id: this.counter,
-        setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-        punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-      };
-      return {
-        cardList: prevCardList.concat(card)
-      };
-    });
+const mapStateToProps = state => {
+  console.log('state>>', state);
+  const cardList = state[namespace];
+  return {
+    cardList
   };
+};
+
+@connect(mapStateToProps)
+export default class PuzzleCardsPage extends Component {
+  // addNewCard = () => {
+  //   this.setState(prevState => {
+  //     const prevCardList = prevState.cardList;
+  //     this.counter += 1;
+  //     const card = {
+  //       id: this.counter,
+  //       setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+  //       punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  //     };
+  //     return {
+  //       cardList: prevCardList.concat(card)
+  //     };
+  //   });
+  // };
 
   render() {
+    const { cardList } = this.props;
+    console.log('cardList>>', cardList);
     return (
       <div>
-        {this.state.cardList.map(card => (
-          <Card key={card.id}>
-            <div>Q: {card.setup}</div>
-            <div>
-              <strong>A: {card.punchline}</strong>
-            </div>
-          </Card>
-        ))}
-        <div>
+        {cardList &&
+          cardList.map(card => (
+            <Card key={card.id}>
+              <div>Q: {card.setup}</div>
+              <div>
+                <strong>A: {card.punchline}</strong>
+              </div>
+            </Card>
+          ))}
+        {/* <div>
           <Button onClick={this.addNewCard}> 添加卡片 </Button>
-        </div>
+        </div> */}
       </div>
     );
   }
