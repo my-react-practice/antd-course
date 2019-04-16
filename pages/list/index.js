f7f5c7624d60433bc14185e4343db2108555c37c
lizhigaomac <lizhigao@021.com>
2019-04-16 20:36:16 +0800

    表单功能

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,9 +1,98 @@
 import React from 'react';
+import { Table, Modal, Button, Form, Input } from 'antd';
+import { connect } from 'dva';
+
+const FormItem = Form.Item;
 
 class List extends React.Component {
+  state = {
+    visible: false
+  };
+  columns = [
+    {
+      title: '名称',
+      dataIndex: 'name'
+    },
+    {
+      title: '描述',
+      dataIndex: 'desc'
+    },
+    {
+      title: '链接',
+      dataIndex: 'url',
+      render: value => <a href={value}>{value}</a>
+    }
+  ];
+
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'cards/queryList'
+    });
+  }
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false
+    });
+  };
+
+  handleOk = () => {
+    const {
+      dispatch,
+      form: { validateFields }
+    } = this.props;
+    validateFields((err, values) => {
+      if (!err) {
+        dispatch({
+          type: 'cards/addOne',
+          payload: values
+        });
+        // 重置 `visible` 属性为 false 以关闭对话框
+        this.setState({ visible: false });
+      }
+    });
+  };
+
   render() {
-    return <div>hello world</div>;
+    const {
+      cardsList,
+      cardsLoading,
+      form: { getFieldDecorator }
+    } = this.props;
+    const { visible } = this.state;
+    return (
+      <div>
+        <Table columns={this.columns} dataSource={cardsList} loading={cardsLoading} rowKey='id' />
+        <Button onClick={this.showModal}>新建</Button>
+        <Modal title='新建记录' visible={visible} onOk={this.handleOk} onCancel={this.handleCancel}>
+          <Form>
+            <FormItem label='名称'>
+              {getFieldDecorator('name', {
+                rules: [{ required: true }]
+              })(<Input />)}
+            </FormItem>
+            <FormItem label='描述'>{getFieldDecorator('desc')(<Input />)}</FormItem>
+            <FormItem label='链接'>
+              {getFieldDecorator('url', {
+                rules: [{ type: 'url' }]
+              })(<Input />)}
+            </FormItem>
+          </Form>
+        </Modal>
+      </div>
+    );
   }
 }
 
-export default List;
+function mapStateToProps(state) {
+  return {
+    cardsList: state.cards.cardsList,
+    cardsLoading: state.loading.effects['cards/queryList']
+  };
+}
+
+export default connect(mapStateToProps)(Form.create()(List));
